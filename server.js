ec7990c42e0745e7060bc0c6b4e9b81af85ce07e
lavrushka <danich002@gmail.com>
2021-04-12 22:21:50 +0700

    New WebSite

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var favicon = require('serve-favicon');
+const app = express();
+const path = require('path');
+const router = express.Router();
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(express.static(__dirname+"/"))
+
+router.get('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/shit', function(req,res){
+  res.sendFile(path.join(__dirname+'/shit.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 80);
+
+console.log('Running at Port 80');

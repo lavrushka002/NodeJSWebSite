1e781c80bedf28103b6cc1fdcb651abebfca8f57
lavrushka002 <danich002@gmail.com>
2021-04-19 19:10:16 +0700

    Minor Changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 const app = express();
 const path = require('path');
 const router = express.Router();
-app.use(favicon(__dirname + '/favicon.ico'));
+//app.use(favicon(__dirname + '/favicon.ico'));
 app.use(express.static(__dirname+"/"))
 
 router.get('/', function(req,res){
@@ -11,8 +11,8 @@ router.get('/', function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/shit', function(req,res){
-  res.sendFile(path.join(__dirname+'/shit.html'));
+router.get('/nextpage', function(req,res){
+  res.sendFile(path.join(__dirname+'/nextpage.html'));
   //__dirname : It will resolve to your project folder.
 });
 

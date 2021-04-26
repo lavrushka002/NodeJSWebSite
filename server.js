65045cb63eed6f308bbdb261f7effb596c8a27dc
lavrushka <danich002@gmail.com>
2021-04-26 14:27:19 +0700

    Nothing Changed

--- server.js
+++ server.js
@@ -16,8 +16,14 @@ router.get('/nextpage', function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+router.get('/auth', function(req,res){
+  res.sendFile(path.join(__dirname+'/auth.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
 //add the router
-app.use('/', router);
+app.use('/auth', router);
 app.listen(process.env.port || 80);
 
 console.log('Running at Port 80');
